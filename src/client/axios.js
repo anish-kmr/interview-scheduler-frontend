208ab0b35afc3ed9e6902bbe53bfe1153b55c225
anish-kmr <anish12k07@gmail.com>
2021-11-18 20:21:58 +0530

    Deployed Backend

--- src/client/axios.js
+++ src/client/axios.js
@@ -1,5 +1,6 @@
 import Axios from "axios";
 export default Axios.create({
-  baseURL: "http://localhost:8080/",
+  // baseURL: "http://localhost:8080/",
+  baseURL: "https://interview-scheduler-backend.herokuapp.com/",
 });
 
